64bfc1680a01062c1b4360d236c1cdee65dec012
oott123 <git@public.oott123.com>
2021-10-15 19:19:26 +0800

    fix: listen port

--- src/config/http.config.ts
+++ src/config/http.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config'
 export default registerAs('http', () => ({
   baseUrl: process.env.HTTP_BASE_URL || '',
   uiUrl: process.env.HTTP_UI_URL || '',
-  port: Number(process.env.HTTP_PORT || process.env.PORT || 3100),
+  port: process.env.HTTP_PORT || process.env.PORT || 3100,
   jwtSecret: process.env.HTTP_JWT_SECRET || '',
   globalPrefix: '/api/v1',
 }))
